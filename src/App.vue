3c885e13f02036f6aba884ae79f6036575bcd8a4
guilherme_j <guilherme_j@grupocriar>
2019-11-06 16:51:53 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes com vuejs</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>
+              Prefixo
+              <span class="badge badge-info">{{ prefixes.length }}</span>
+            </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li
+                    class="list-group-item"
+                    v-for="prefix in prefixes"
+                    v-bind:key="prefix"
+                  >{{prefix}}</li>
+                </ul>
+                <br />
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Digete o prefixo"
+                    v-model="prefix"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addPrefix(prefix)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>
+              Sufixo
+              <span class="badge badge-info">{{ sufixes.length }}</span>
+            </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li
+                    class="list-group-item"
+                    v-for="sufix in sufixes"
+                    v-bind:key="sufix"
+                  >{{ sufix }}</li>
+                </ul>
+                <br />
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Digete o sufixo"
+                    v-model="sufix"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addSufix(sufix)">
+                      <span class="fa fa-plus"></span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <h5>
+          Domains
+          <span class="badge badge-info">{{ domains.length }}</span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{ domain }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+export default {
+  name: "app",
+  data: () => {
+    return {
+      prefix: "",
+      sufix: "",
+      prefixes: ["Air", "Jet", "Flight"],
+      sufixes: ["Hub", "Station", "Mart"],
+      domains: []
+    };
+  },
+  methods: {
+    addPrefix(prefix) {
+      this.prefixes.push(prefix);
+      this.prefix = "";
+      this.generator();
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+      this.sufix = "";
+      this.generator();
+    },
+    generator() {
+      this.domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          this.domains.push(prefix + sufix);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+}
+#main {
+  background: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
